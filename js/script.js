bd1d88e19e738a1882e7bce9fd00f2ed6c3ccc82
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-05-10 14:35:09 -0300

    Eu criei uma função que cria elementos de um formulario, e criei outra função que captura o valor que o usuario digitar no campo de texto quando o botao for clicado.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,30 @@ const handleClick = (event) => {
 };
 
 navLinks.forEach((item) => item.addEventListener("click", handleClick));
+
+const createTasksAll = (conteudo, atritbuto) => {
+  const label = document.createElement("label");
+  label.innerText = conteudo;
+  label.setAttribute("for", atritbuto);
+  const task = `<div>
+  <input type='checkbox' name='${atritbuto}' id='${atritbuto}'>
+  ${label.outerHTML}
+  </div>
+  `;
+  return task;
+};
+
+const btnEnviar = document.querySelector(".btn-enviar");
+const formToDoList = document.querySelector("#form-toDoList");
+
+const valueFormTask = (event) => {
+  event.preventDefault();
+  const valueText = formToDoList[0].value;
+  const numRandom = Math.random().toFixed(7);
+  return {
+    valueText,
+    numRandom,
+  };
+};
+
+btnEnviar.addEventListener("click", valueFormTask);
