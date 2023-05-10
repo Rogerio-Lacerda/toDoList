a8b8c409cbf78829879fb087c06b0786134c536a
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-05-10 15:40:09 -0300

    Eu criei uma função para setar os valores digitados pelo usuário no container de conteúdo.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 const navLinks = document.querySelectorAll("[data-nav]");
 
-const handleClick = (event) => {
+const clickNav = (event) => {
   event.preventDefault();
   const link = event.target;
   navLinks.forEach((item) => item.classList.remove("active"));
   link.classList.add("active");
 };
+navLinks.forEach((item) => item.addEventListener("click", clickNav));
 
-navLinks.forEach((item) => item.addEventListener("click", handleClick));
-
-const createTasksAll = (conteudo, atritbuto) => {
+const createTask = (conteudo, atritbuto) => {
   const label = document.createElement("label");
   label.innerText = conteudo;
   label.setAttribute("for", atritbuto);
@@ -24,14 +23,36 @@ const createTasksAll = (conteudo, atritbuto) => {
 const btnEnviar = document.querySelector(".btn-enviar");
 const formToDoList = document.querySelector("#form-toDoList");
 
-const valueFormTask = (event) => {
+const setandoTarefas = (valoresForm) => {
+  const valoresTask = valoresForm;
+  const conteudoDiv = createTask(valoresTask.valueText, valoresTask.numRandom);
+  const container = document.querySelector(".form-tarefas");
+  if (valoresTask.valueText === "") {
+    const erroAll = document.querySelectorAll(".erro");
+    if (erroAll.length) {
+      erroAll.forEach((item) => item.remove());
+    }
+    const erro = document.createElement("span");
+    erro.innerText = "Preencha o campo de texto!";
+    erro.classList.add("erro");
+    formToDoList.appendChild(erro);
+  } else {
+    const erro = document.querySelector(".erro");
+    if (erro) {
+      erro.remove();
+    }
+    container.innerHTML += conteudoDiv;
+  }
+};
+
+const valoresTarefas = (event) => {
   event.preventDefault();
   const valueText = formToDoList[0].value;
-  const numRandom = Math.random().toFixed(7);
-  return {
+  const numRandom = "_" + Math.random().toFixed(7);
+  const valoresObj = {
     valueText,
     numRandom,
   };
+  setandoTarefas(valoresObj);
 };
-
-btnEnviar.addEventListener("click", valueFormTask);
+btnEnviar.addEventListener("click", valoresTarefas);
